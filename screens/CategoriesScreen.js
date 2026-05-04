8139380d667573dd58aab3ab84330290d60357f4
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-04 10:50:52 +0530

    Getting Started with the App & Outputting Meal Categories

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+export default function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
+}
+
+const styles = StyleSheet.create({});
