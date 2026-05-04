e1f3983122c8949d1fdfe59043c29ea45e8bd019
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-04 19:53:21 +0530

    working with route parameters to pass data between screens

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
-export default function MealsOverviewScreen() {
+export default function MealsOverviewScreen({ route }) {
+  const id = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen: {id}</Text>
     </View>
   );
 }
