bbbb5c657b1e5a72f9e67318318bf049a9397e3a
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-05 16:50:15 +0530

    Adding an Icon Button to a Header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -11,6 +11,7 @@ import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 
 export default function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -22,7 +23,13 @@ export default function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title="Tap me!" onPress={headerButtonPressHandler} />;
+        return (
+          <IconButton
+            name="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
       },
     });
   }, [navigation, headerButtonPressHandler]);
